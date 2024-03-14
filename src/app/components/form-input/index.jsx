0c698e6a3d956258fdbf12729d49c3d4e008b9dd
Pedro-Dom <pedroaugusto.dom@outlook.com>
2024-03-14 00:56:00 -0300

    depois de várias coringadas, add seções no form

--- src/app/components/form-input/index.jsx
+++ src/app/components/form-input/index.jsx
@@ -1,11 +1,21 @@
 import styles from "./input-form.module.scss";
 
-export default function FormInput({type, label}) {
+export default function FormInput({type, label, value, onChange}) {
+  const handleChange = (e) => {
+    onChange(e, label);
+  }
 
   return (
     <div  className={styles.inputWithLabel}>
       <label className={styles.labelForm} htmlFor="">{label}</label>
-      <input className={styles.inputForm} type={type} placeholder={label}/>
+      <input 
+        className={styles.inputForm} 
+        name={label} 
+        type={type} 
+        placeholder={label}
+        value={value}
+        onChange={handleChange}
+      />
     </div>
   );
 }
--- src/app/components/form/index.jsx
+++ src/app/components/form/index.jsx
@@ -1,12 +1,110 @@
+"use client"
+
+import { useState } from 'react';
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import FormInput from "@/app/components/form-input";
 import styles from "./form.module.scss";
 
-import FormInput from "@/app/components/form-input";
-
 export default function Form() {
+
+  const [section, setSection] = useState(0); // Inicializando o estado de section com o valor 0
+  const [formData, setFormData] = useState({
+    Nome: '',
+    Telefone: '',
+    Batizado: '',
+    Email: '',
+    Casada: '',
+    Igreja: '', 
+    Jovem: ''
+  });
+  
+  let formFields;
+
+  switch(section) {
+    case  0:
+      formFields = [
+        {
+          label: 'Nome',
+          type: 'text'
+        },
+        {
+          label: 'Telefone',
+          type: 'number'
+        },
+      ];
+      break;
+    case 1:
+      formFields = [
+        {
+          label: 'Batizado',
+          type: 'text'
+        },
+        {
+          label: 'Email',
+          type: 'text'
+        },
+        {
+          label: 'Casada',
+          type: 'text'
+        }
+      ];
+      break;
+    case 2:
+      formFields = [
+        {
+          label: 'Igreja',
+          type: 'text'
+        },
+        {
+          label: 'Jovem',
+          type: 'text'
+        }
+      ];
+      break;
+  }
+
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value
+    }));
+  }
+
+  const renderForms = () => {
+    return formFields.map((field, f) => (
+      <FormInput 
+        key={f} 
+        type={field.type} 
+        label={field.label}
+        value={formData[field.label]}
+        onChange={handleInputChange}
+      />
+    ));
+  }
+
+  const saveData = () => {
+    console.log(formData);
+  }
+
+  const beforeSection = () => {
+    if (!section) return;
+    setSection(section - 1);
+  }
+
+  const nextSection = () => {
+    if (section == 2) return;
+    saveData();
+    setSection(section + 1);
+  }
+
   return (
     <main className={styles.formTemplate}>
-      <FormInput type='text' label='Nome'/>
-      <FormInput type='number' label='Telefone'/>
+      {renderForms()}
+      <div className={styles.chevronControllers}>
+        <FaAngleLeft onClick={beforeSection} className={!section ? styles.disabled : ''}/>
+        <FaAngleRight onClick={nextSection} className={section == 2 ? styles.disabled : ''}/>
+      </div>
     </main>
   );
 }
